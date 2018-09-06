b1255c7cea54ac4b5a5308e03ae77b2e1629fdea
TheRealMerlin <iankuffel@gmail.com>
2018-09-06 15:25:12 -0500

    Nicholas Working

--- Written Tests/2016 District 1/src/Nicholas.java
+++ Written Tests/2016 District 1/src/Nicholas.java
@@ -2,34 +2,51 @@ import java.util.*;
 import java.io.*;
 import static java.lang.System.*;
 
-/* String adding solution IN PROGRESS
- * Matrix Multiplication is easier
- */
 public class Nicholas {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(new File("nicholas.dat"));
 		while(in.hasNext()) {
 			int n = in.nextInt();
 			int p = in.nextInt();
-			HashMap<String, Boolean> map = new HashMap<String, Boolean>();
-			char defC = 'A';
-			char c = 'A';
+			int[][] arr = new int[n][n];
 			for(int i = 0; i < n; i++) {
 				String str = in.next();
 				for(int j = 0; j < n; j++) {
-					map.put(Character.toString(c) + Character.toString((char)(defC + j)), getBool(str.substring(j, j+1)));
+					arr[i][j] = Integer.parseInt(str.substring(j, j+1));
 				}
-				c++;
 			}
-			out.println(map);
+			arr = Mult(arr, arr, p);
+			out.println(Count(arr));
 		}
 		in.close();
 	}
 	
-	public static Boolean getBool(String str) {
-		if(str.equals("1")) {
-			return true;
+	public static int[][] Mult(int[][] arr, int[][] bArr, int p) {
+		if(p == 1) {
+			return arr;
 		}
-		return false;
+		int[][] temp = new int[arr.length][arr.length];
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length; j++) {
+				for(int k = 0; k < arr.length; k++) {
+					temp[i][j] += arr[i][k] * bArr[k][j];
+				}
+			}
+		}
+		p--;
+		if(p > 1) {
+			temp = Mult(temp, bArr, p);
+		}
+		return temp;
+	}
+	
+	public static int Count(int[][] arr) {
+		int res = 0;
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length; j++) {
+				res += arr[i][j];
+			}
+		}
+		return res;
 	}
 }
