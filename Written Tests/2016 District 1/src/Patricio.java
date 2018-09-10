55465a901116110716d583b9ebbee8f83fc231ba
TheRealMerlin <iankuffel@gmail.com>
2018-09-09 23:48:47 -0500

    Patricio Takes Too Long

--- Written Tests/2016 District 1/src/Patricio.java
+++ Written Tests/2016 District 1/src/Patricio.java
@@ -8,42 +8,40 @@ public class Patricio {
 		Scanner in = new Scanner(new File("patricio.dat"));
 		ScriptEngine engine = new ScriptEngineManager().getEngineByName("javascript");
 		int amt = in.nextInt();
-		for(int i = 0; i < amt; i++) {
+		while(amt-- > 0) {
 			in.nextLine();
 			String eq = in.nextLine();
 			double maxX = in.nextDouble();
 			double maxY = in.nextDouble();
 			int maxT = in.nextInt();
 			eq = eq.substring(4);
-			Object[] res = FindMaxZ(engine, eq, maxX, maxY, maxT);
-			out.printf("Patricio should launch at x=%.1f and y=%.1f at t=%d at a height of %.1f.", res[0], res[1], res[2], res[3]);
+			eq = AddMult(eq);
+			eq = ReplacePower(eq);
+			Object[] res = {0.0, 0.0, 0, 0.0};
+			for(double x = 0; x <= maxX; x+=0.1) {
+				for(double y = 0; y <= maxY; y+=0.1) {
+					for(int t = 0; t <= maxT; t++) {
+						String temp = eq;
+						temp = temp.replaceAll("Math", "Maah");
+						temp = temp.replaceAll("x", Double.toString(x));
+						temp = temp.replaceAll("y", Double.toString(y));
+						temp = temp.replaceAll("t", Integer.toString(t));
+						temp = temp.replaceAll("Maah", "Math");
+						double z = (double)engine.eval(temp);
+						if(!(z >= Double.MAX_VALUE) && z > (double)res[3]) {
+							res[0] = x;
+							res[1] = y;
+							res[2] = t;
+							res[3] = z;
+						}
+					}
+				}
+			}
+			out.printf("Patricio should launch at x=%.1f and y=%.1f at t=%d at a height of %.1f.\n", res[0], res[1], res[2], res[3]);
 		}
 		in.close();
 	}
 	
-	public static Object[] FindMaxZ(ScriptEngine engine, String eq, double maxX, double maxY, int maxT) throws ScriptException {
-		Object[] res = {0.0, 0.0, 0, 0.0};
-		for(double x = maxX; x <= maxX; x-=0.1) {
-			for(double y = maxY; y <= maxY; y-=0.1) {
-				for(int t = maxT; t <= maxT; t--) {
-					String temp = eq;
-					temp = ReplaceVal(temp, "x", x);
-					temp = ReplaceVal(temp, "y", y);
-					temp = ReplaceVal(temp, "t", t);
-					temp = ReplacePower(temp);
-					Object z = engine.eval(temp);
-					if((double)z > (double)res[3]) {
-						res[0] = x;
-						res[1] = y;
-						res[2] = t;
-						res[3] = (double)z;
-					}
-				}
-			}
-		}
-		return res;
-	}
-
 	public static int findNextOp(String str, int loc) {
 		int[] arr = new int[4];
 		int res = 0;
@@ -76,57 +74,34 @@ public class Patricio {
 	}
 	
 	public static String ReplacePower(String eq) {
-		String[] temp = eq.split(" ");
-		for(int i = 0; i < temp.length; i++) {
-			if(!(temp[i].equals("+") || temp[i].equals("-") || temp[i].equals("/") || temp[i].equals("*"))) {
-				if(temp[i].contains("^")) {
-					int loc = temp[i].indexOf("^");
-					int firstOp = findFirstOp(temp[i], loc);
-					int nextOp = findNextOp(temp[i], loc);
-					if(firstOp > 0 && nextOp > 0) {
-						temp[i] = temp[i].substring(0, firstOp+1) + "Math.pow((" + temp[i].substring(firstOp+1, loc) + ")," + "(" + temp[i].substring(loc+1, nextOp) + "))" + temp[i].substring(nextOp);
-					}else if(firstOp > 0) {
-						temp[i] = temp[i].substring(0, firstOp+1) + "Math.pow((" + temp[i].substring(firstOp+1, loc) + ")," + "(" + temp[i].substring(loc+1) + "))";
-					}else if(nextOp > 0) {
-						temp[i] = "Math.pow((" + temp[i].substring(0, loc) + ")," + "(" + temp[i].substring(loc+1, nextOp) + "))" + temp[i].substring(nextOp);
-					}else {
-						temp[i] = "Math.pow((" + temp[i].substring(0, loc) + ")," + "(" + temp[i].substring(loc+1) + "))";
-					}
+		String[] arr = eq.split(" ");
+		for(int i = 0; i < arr.length; i++) {
+			if(!(arr[i].equals("+") || arr[i].equals("-") || arr[i].equals("*") || arr[i].equals("/")) && arr[i].contains("^")) {
+				int loc = arr[i].indexOf("^");
+				int firstOp = findFirstOp(arr[i], loc);
+				int nextOp = findNextOp(arr[i], loc);
+				if(firstOp > 0 && nextOp > 0) {
+					arr[i] = arr[i].substring(0, firstOp+1) + "Math.pow((" + arr[i].substring(firstOp+1, loc) + ")," + "(" + arr[i].substring(loc+1, nextOp) + "))" + arr[i].substring(nextOp);
+				}else if(firstOp > 0) {
+					arr[i] = arr[i].substring(0, firstOp+1) + "Math.pow((" + arr[i].substring(firstOp+1, loc) + ")," + "(" + arr[i].substring(loc+1) + "))";
+				}else if(nextOp > 0) {
+					arr[i] = "Math.pow((" + arr[i].substring(0, loc) + ")," + "(" + arr[i].substring(loc+1, nextOp) + "))" + arr[i].substring(nextOp);
+				}else {
+					arr[i] = "Math.pow((" + arr[i].substring(0, loc) + ")," + "(" + arr[i].substring(loc+1) + "))";
 				}
 			}
 		}
-		return String.join(" ", temp);
+		return String.join(" ", arr);
 	}
 	
-	public static String ReplaceVal(String eq, String let, Object val) {
-		if(val instanceof Integer) {
-			while(eq.contains(let)) {
-				int loc = eq.indexOf(let);
-				if(loc > 0 && isNumeric(eq.substring(loc-1, loc))) {
-					eq = eq.substring(0, loc) + "*" + eq.substring(loc).replaceFirst(let, Integer.toString((int)val));
-				}else {
-					eq = eq.replaceFirst(let, Integer.toString((int)val));
+	public static String AddMult(String eq) {
+		for(int i = 0; i < eq.length(); i++) {
+			try {
+				if((Character.isAlphabetic(eq.charAt(i)) && (Character.isDigit(eq.charAt(i+1)) || Character.isAlphabetic(eq.charAt(i+1)))) || (Character.isDigit(eq.charAt(i)) && Character.isAlphabetic(eq.charAt(i+1)))) {
+					eq = eq.substring(0, i+1) + "*" + eq.substring(i+1);
 				}
-			}
-		}else {
-			while(eq.contains(let)) {
-				int loc = eq.indexOf(let);
-				if(loc > 0 && isNumeric(eq.substring(loc-1, loc))) {
-					eq = eq.substring(0, loc) + "*" + eq.substring(loc).replaceFirst(let, Double.toString((double)val));
-				}else {
-					eq = eq.replaceFirst(let, Double.toString((double)val));
-				}
-			}
+			}catch(Exception e) {}
 		}
 		return eq;
-	}
-	
-	public static Boolean isNumeric(String str) {
-		try {
-			int a = Integer.parseInt(str);
-		}catch(Exception e) {
-			return false;
-		}
-		return true;
 	}
 }
