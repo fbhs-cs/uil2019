517124a15387c8f27f6f71d7bc50b39ee7978fa4
TheRealMerlin <iankuffel@gmail.com>
2018-09-17 15:36:20 -0500

    Revisions to Madison

--- Written Tests/2016 District 1/src/Madison.java
+++ Written Tests/2016 District 1/src/Madison.java
@@ -8,17 +8,27 @@ public class Madison {
 		Scanner in = new Scanner(new File("madison.dat"));
 		ScriptEngine engine = new ScriptEngineManager().getEngineByName("javascript");
 		while(in.hasNext()) {
-			char c = 'A';
 			String eq = in.next();
-			String val = in.next();
-			for(int i = 0; i < val.length(); i++) {
-				eq = eq.replaceAll(Character.toString(c), val.substring(i, i+1));
-				c++;
+			String vals = in.next();
+			boolean[] arr = new boolean[3];
+			for(int i = 0; i < vals.length(); i++) {
+				arr[i] = vals.substring(i, i+1).equals("1");
 			}
-			eq = eq.replaceAll("\\+", " || ");
-			eq = eq.replaceAll("\\*", " && ");
+			eq = eq.replaceAll("A", String.valueOf(arr[0]));
+			eq = eq.replaceAll("B", String.valueOf(arr[1]));
+			eq = eq.replaceAll("C", String.valueOf(arr[2]));
+			eq = eq.replaceAll("\\+", "||");
+			eq = eq.replaceAll("\\*", "&&");
 			Object obj = engine.eval(eq);
-			out.println(obj == (Object)1);
+			if(obj instanceof Integer) {
+				if((int)obj == 1) {
+					out.println(true);
+				}else {
+					out.println(false);
+				}
+			}else {
+				out.println(obj);
+			}
 		}
 		in.close();
 	}
