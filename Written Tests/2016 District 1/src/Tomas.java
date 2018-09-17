8917f8b1267325168aa4994f15072fba144d4112
TheRealMerlin <iankuffel@gmail.com>
2018-09-17 15:56:53 -0500

    Revision to Tomas

--- Written Tests/2016 District 1/src/Tomas.java
+++ Written Tests/2016 District 1/src/Tomas.java
@@ -1,14 +1,15 @@
 import java.util.*;
 import java.io.*;
+import java.math.*;
 import static java.lang.System.*;
 
 public class Tomas {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(new File("tomas.dat"));
-		int[] arr = new int[50];
-		for(int i = 0; i < 3; i++) arr[i] = 1;
+		BigInteger[] arr = new BigInteger[50];
+		for(int i = 0; i < 3; i++) arr[i] = BigInteger.valueOf(1);
 		for(int i = 3; i < 50; i++) {
-			arr[i] = arr[i-3] + arr[i-2] + arr[i-1];
+			arr[i] = arr[i-3].add(arr[i-2]).add(arr[i-1]);
 		}
 		while(in.hasNext()) {
 			out.println(arr[in.nextInt()-1]);
