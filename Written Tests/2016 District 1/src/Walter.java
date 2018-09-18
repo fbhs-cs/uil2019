3acef82770fcb7e7b1fcfebdd0fd85e1fd497661
TheRealMerlin <iankuffel@gmail.com>
2018-09-18 16:09:36 -0500

    Walter IN PROGRESS

---
+++ Written Tests/2016 District 1/src/Walter.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import static java.lang.System.*;
+
+public class Walter {
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(new File("walter.dat"));
+		while(in.hasNext()) {
+			double l = in.nextDouble();
+			double w = in.nextDouble();
+			double d = in.nextDouble();
+			if(l >= 3.5 && l <= 4.25 && w >= 3.5 && w <= 6 && d >= .007 && d <= .016) {
+				out.println("SMALL POST CARD");
+			}else if(l >= 4.25 && l <= 6 && w >= 6 && w <= 11.5 && d >= .007 && d <= .016) {
+				out.println("LARGE POST CARD");
+			}else if(l >= 3.5 && l <= 6.125 && w >= 5 && w <= 11.5 && d >= .016 && d <= .25) {
+				out.println("SMALL ENVELOPE");
+			}else if(l >= 6.125 && l <= 24 && w >= 3.5 && w <= 6 && d >= .007 && d <= .016) {
+				out.println("LARGE ENVELOPE");
+			}else if(l >= 3.5 && l <= 4.25 && w >= 3.5 && w <= 6 && d >= .007 && d <= .016) {
+				out.println("SMALL POST CARD");
+			}else if(l >= 3.5 && l <= 4.25 && w >= 3.5 && w <= 6 && d >= .007 && d <= .016) {
+				out.println("SMALL POST CARD");
+			}else {
+				out.println("UNMAILABLE");
+			}
+		}
+		in.close();
+	}
+}
