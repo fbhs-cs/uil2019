c49ebbd6145ff95da9146a628129eb4f1ecb3cc3
TheRealMerlin <iankuffel@gmail.com>
2018-09-19 15:26:52 -0500

    Walter WORKING

--- Written Tests/2016 District 1/src/Walter.java
+++ Written Tests/2016 District 1/src/Walter.java
@@ -15,12 +15,12 @@ public class Walter {
 				out.println("LARGE POST CARD");
 			}else if(l >= 3.5 && l <= 6.125 && w >= 5 && w <= 11.5 && d >= .016 && d <= .25) {
 				out.println("SMALL ENVELOPE");
-			}else if(l >= 6.125 && l <= 24 && w >= 3.5 && w <= 6 && d >= .007 && d <= .016) {
+			}else if(l >= 6.125 && l <= 24 && w >= 11 && w <= 18 && d >= .25 && d <= .5) {
 				out.println("LARGE ENVELOPE");
-			}else if(l >= 3.5 && l <= 4.25 && w >= 3.5 && w <= 6 && d >= .007 && d <= .016) {
-				out.println("SMALL POST CARD");
-			}else if(l >= 3.5 && l <= 4.25 && w >= 3.5 && w <= 6 && d >= .007 && d <= .016) {
-				out.println("SMALL POST CARD");
+			}else if(l > 24 && w > 18 && d > .5 && l + 2*w + 2*d <= 84) {
+				out.println("SMALL PACKAGE");
+			}else if(l + 2*w + 2*d <= 130 && l + 2*w + 2*d >= 84) {
+				out.println("LARGE PACKAGE");
 			}else {
 				out.println("UNMAILABLE");
 			}
