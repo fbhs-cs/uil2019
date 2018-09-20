410ef17f7e5c946794d062841cc86f503071afdf
TheRealMerlin <iankuffel@gmail.com>
2018-09-20 15:17:42 -0500

    Yash WORKING

--- Written Tests/2016 District 1/src/Yash.java
+++ Written Tests/2016 District 1/src/Yash.java
@@ -1,35 +1,21 @@
 import java.util.*;
 import java.io.*;
+import java.text.*;
 import static java.lang.System.*;
 
 public class Yash {
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(new File("yash.dat"));
+		NumberFormat myformat = NumberFormat.getInstance();
+		myformat.setGroupingUsed(true);
 		while(in.hasNext()) {
 			String str = in.next();
 			str.replaceAll(",", "");
 			int num = Integer.parseInt(str);
 			int x = 0;
 			while(Math.pow(2, x) < num) x++;
-			out.println("1 " + toString(x) + " " + toString(num) + " " + toString(num*x) + " " + toString(num*num));
+			out.println("1 " + myformat.format(x) + " " + myformat.format(num) + " " + myformat.format(x*num) + " " + myformat.format(num*num));
 		}
 		in.close();
-	}
-	
-	public static String toString(int num) {
-		StringBuilder str = new StringBuilder();
-		str.append(Integer.toString(num));
-		str.reverse();
-		StringBuilder res = new StringBuilder();
-		int c = 0;
-		while(c < str.length()) {
-			if(c != 0 && c % 3 == 0) {
-				res.append(",");
-			}else {
-				res.append(str.substring(c, c+1));
-			}
-		}
-		res.reverse();
-		return res.toString();
 	}
 }
