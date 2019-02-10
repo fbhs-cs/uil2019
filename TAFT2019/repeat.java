8f27b740a4526ab0bdf38093cef5eb18e3cd9b55
Chad Purdy <chpurdy@gmail.com>
2019-02-10 15:26:36 -0600

    suffix array class created

--- TAFT2019/repeat.java
+++ TAFT2019/repeat.java
@@ -1,2 +1,30 @@
 import java.io.*;
 import java.util.*;
+
+public class repeat {
+
+    public static class SuffixArray {
+        ArrayList<String> array;
+
+        public SuffixArray(String str) {
+            for (int i = 0; i < str.length(); i++) {
+                array.add(str.substring(i));
+            }
+            Collections.sort(array);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("repeat.dat"));
+        int n = in.nextInt();
+        while (n-- > 0) {
+            String str = in.next();
+            SuffixArray array = new SuffixArray(str);
+            for (String s : array.array) {
+                System.out.println(s);
+            }
+        }
+
+    }
+}
