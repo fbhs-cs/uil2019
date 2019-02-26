b4325c71c96c8b260f7394ba2bcf7b3dc3f53eb8
Chad Purdy <chpurdy@gmail.com>
2019-02-26 15:27:18 -0600

    Add files via upload

--- Callalen/HikingChallenge.java
+++ Callalen/HikingChallenge.java
@@ -4,7 +4,7 @@ import static java.lang.System.*;
 
 public class HikingChallenge {
     public static int heuristic(Node a, Node b) {
-        return(Math.abs(a.i-b.i) + Math.abs(a.j-b.j));
+        return (Math.abs(a.i-b.i) + Math.abs(a.j-b.j));
     }
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(new File("hiking_challenge.dat"));
@@ -25,9 +25,11 @@ public class HikingChallenge {
                     grid[i][j].getNeighbors(grid);
                 }
             }
-            Node start = grid[Integer.parseInt(first.substring(2, first.indexOf(",")))][Integer.parseInt(first.substring(first.indexOf(",")+1, first.indexOf(")")))];
-            Node end = grid[Integer.parseInt(first.substring(first.lastIndexOf("(")+1, first.lastIndexOf(",")))][Integer.parseInt(first.substring(first.lastIndexOf(",")+1, first.lastIndexOf(")")))];
-            start.g = 0;
+
+            // use split...your i and j represent ROWS then COLUMNS...the input is COLUMNS then ROWS
+            Node start = grid[Integer.parseInt(first.substring(first.indexOf(",")+1, first.indexOf(")")))][Integer.parseInt(first.substring(2, first.indexOf(",")))];
+            Node end = grid[Integer.parseInt(first.substring(first.lastIndexOf(",")+1, first.lastIndexOf(")")))][Integer.parseInt(first.substring(first.lastIndexOf("(")+1, first.lastIndexOf(",")))];
+            start.g = start.h; // count the cost to go through the starting node
             start.f = heuristic(start, end);
             openSet.add(start);
             Node current;
@@ -43,19 +45,14 @@ public class HikingChallenge {
                 openSet.remove(current);
                 closedSet.add(current);
 
-                path = new ArrayList<Node>();
-                Node temp = current;
-                path.add(temp);
-                while(temp.prev != null) {
-                    path.add(temp.prev);
-                    temp = temp.prev;
-                }
+                
 
                 for(Node neighbor : current.neighbors) {
                     if(closedSet.contains(neighbor)) {
                         continue;
                     }
-                    int tempG = current.g + 1;
+                    // calculate the potential cost to get through the neighbor node
+                    int tempG = current.g + neighbor.h;
                     if(!openSet.contains(neighbor)) {
                         openSet.add(neighbor);
                     }else if(tempG > neighbor.g) {
@@ -63,10 +60,19 @@ public class HikingChallenge {
                     }
                     neighbor.prev = current;
                     neighbor.g = tempG;
-                    neighbor.f = neighbor.g + heuristic(neighbor, end) + neighbor.h;
+                    // approximate cost to get through neighbor
+                    neighbor.f = neighbor.g + heuristic(neighbor, end);
                 }
             }while(current != end);
 
+
+            path = new ArrayList<Node>();
+                Node temp = current;
+                path.add(temp);
+                while(temp.prev != null) {
+                    path.add(temp.prev);
+                    temp = temp.prev;
+                }
             for(int i = 0; i < grid.length; i++) {
                 String line  = "";
                 for(int j = 0; j < grid[i].length; j++) {
@@ -79,7 +85,7 @@ public class HikingChallenge {
                 time += path.get(i).h;
                 out.println(path.get(i));
             }
-            out.printf("Your time to beat is %d minutes.\n", time);
+            out.printf("Your time to beat is %d minutes.\n", current.g-current.h);
         }
     }
 }
