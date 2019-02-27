73ebb8a20b8673d75eb2d9ff6b91fcaf7663c94f
Ian Kuffel <40146929+TheRealMerlin@users.noreply.github.com>
2019-02-26 20:37:45 -0600

    Fixed

--- Callalen/HikingChallenge.java
+++ Callalen/HikingChallenge.java
@@ -3,9 +3,6 @@ import java.io.*;
 import static java.lang.System.*;
 
 public class HikingChallenge {
-    public static int heuristic(Node a, Node b) {
-        return(Math.abs(a.i-b.i) + Math.abs(a.j-b.j));
-    }
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(new File("hiking_challenge.dat"));
         while (in.hasNextLine()) {
@@ -13,74 +10,77 @@ public class HikingChallenge {
             Node[][] grid = new Node[5][5];
             ArrayList<Node> openSet = new ArrayList<Node>();
             ArrayList<Node> closedSet = new ArrayList<Node>();
-            ArrayList<Node> path = new ArrayList<Node>();
+            ArrayList<Node> path;
+
             for (int i = 0; i < 5; i++) {
                 char[] line = in.nextLine().toCharArray();
                 for (int j = 0; j < 5; j++) {
                     grid[i][j] = new Node(i, j, line[j]);
                 }
             }
+
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
                     grid[i][j].getNeighbors(grid);
                 }
             }
-            Node start = grid[Integer.parseInt(first.substring(2, first.indexOf(",")))][Integer.parseInt(first.substring(first.indexOf(",")+1, first.indexOf(")")))];
-            Node end = grid[Integer.parseInt(first.substring(first.lastIndexOf("(")+1, first.lastIndexOf(",")))][Integer.parseInt(first.substring(first.lastIndexOf(",")+1, first.lastIndexOf(")")))];
+
+            Node start = grid[Integer.parseInt(first.substring(first.indexOf(",") + 1, first.indexOf(")")))][Integer.parseInt(first.substring(2, first.indexOf(",")))];
+            Node end = grid[Integer.parseInt(first.substring(first.lastIndexOf(",") + 1, first.lastIndexOf(")")))][Integer.parseInt(first.substring(first.lastIndexOf("(") + 1, first.lastIndexOf(",")))];
+            start.f = heuristic(start, end) + start.h;
             start.g = 0;
-            start.f = heuristic(start, end);
             openSet.add(start);
             Node current;
 
             do {
                 current = openSet.get(0);
-                for(int i = 1; i < openSet.size(); i++) {
-                    if(openSet.get(i).f < current.f) {
+                int lowest = current.f;
+                for (int i = 1; i < openSet.size(); i++) {
+                    if (openSet.get(i).f < lowest) {
                         current = openSet.get(i);
+                        lowest = current.f;
                     }
                 }
 
-                openSet.remove(current);
                 closedSet.add(current);
+                openSet.remove(current);
 
-                path = new ArrayList<Node>();
-                Node temp = current;
-                path.add(temp);
-                while(temp.prev != null) {
-                    path.add(temp.prev);
-                    temp = temp.prev;
-                }
-
-                for(Node neighbor : current.neighbors) {
-                    if(closedSet.contains(neighbor)) {
+                for (Node neighbor : current.neighbors) {
+                    if (closedSet.contains(neighbor)) {
                         continue;
                     }
-                    int tempG = current.g + 1;
-                    if(!openSet.contains(neighbor)) {
+
+                    int tempG = current.g + neighbor.h + 1;
+
+                    if (!openSet.contains(neighbor)) {
                         openSet.add(neighbor);
-                    }else if(tempG > neighbor.g) {
+                    } else if (tempG >= neighbor.g) {
                         continue;
                     }
                     neighbor.prev = current;
                     neighbor.g = tempG;
-                    neighbor.f = neighbor.g + heuristic(neighbor, end) + neighbor.h;
+                    neighbor.f = neighbor.g + heuristic(neighbor, end);
+                }
+
+                path = new ArrayList<Node>();
+                Node temp = current;
+                path.add(temp);
+                while (temp.prev != null) {
+                    path.add(temp.prev);
+                    temp = temp.prev;
                 }
             }while(current != end);
 
-            for(int i = 0; i < grid.length; i++) {
-                String line  = "";
-                for(int j = 0; j < grid[i].length; j++) {
-                    line = String.join(" ", line, grid[i][j].f+"");
-                }
-                out.println(line);
-            }
             int time = 0;
-            for(int i = 1; i < path.size(); i++) {
+            for (int i = 1; i < path.size(); i++) {
                 time += path.get(i).h;
-                out.println(path.get(i));
             }
             out.printf("Your time to beat is %d minutes.\n", time);
         }
+    }
+
+    public static int heuristic(Node a, Node b) {
+        return (Math.abs(a.i - b.i) + Math.abs(a.j - b.j));
     }
 }
 
@@ -94,31 +94,27 @@ class Node {
         this.i = i;
         this.j = j;
         this.type = type;
-        switch(type) {
-            case 'T': this.h = 3; break;
-            case 'W': this.h = 8; break;
-            case 'O': this.h = 12; break;
-            case 'S': this.h = 20; break;
+        switch (type) {
+        case 'T': this.h = 3; break;
+        case 'W': this.h = 8; break;
+        case 'O': this.h = 12; break;
+        default: this.h = 20; break;
         }
-        neighbors = new ArrayList<Node>();
+        this.neighbors = new ArrayList<Node>();
     }
 
     public void getNeighbors(Node[][] grid) {
-        if (i > 0) {
+        if (this.i > 0) {
             neighbors.add(grid[i - 1][j]);
         }
-        if (j > 0) {
+        if (this.j > 0) {
             neighbors.add(grid[i][j - 1]);
         }
-        if (i < grid.length - 1) {
+        if (this.i < grid.length - 1) {
             neighbors.add(grid[i + 1][j]);
         }
-        if (j < grid[i].length - 1) {
+        if (this.j < grid[i].length - 1) {
             neighbors.add(grid[i][j + 1]);
         }
-    }
-
-    public String toString() {
-        return(i + ", " + j);
     }
 }
